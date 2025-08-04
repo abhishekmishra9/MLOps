9348c4cf061f092bf328cb953dc73f25e76ef9f1
abhishek <abhi.rift@gmail.com>
2025-08-05 02:57:34 +0530

    changes 2

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 
 # Prometheus metrics
 REQUEST_COUNT = Counter("api_requests_total", "Total number of requests")
-PREDICTION_COUNT = Counter("prediction_requests_total", "Total prediction requests")
+PREDICTION_COUNT =Counter("prediction_requests_total", "Total prediction requests")
 ERROR_COUNT = Counter("prediction_errors_total", "Total prediction errors")
 RETRAIN_COUNT = Counter("retrain_requests_total", "Total retrain requests")
 
@@ -30,6 +30,7 @@ RETRAIN_COUNT = Counter("retrain_requests_total", "Total retrain requests")
 MODEL_PATH = os.path.join("models", "best_model", "model.pkl")
 
 # Input schema
+
 class HousingInput(BaseModel):
     MedInc: float
     HouseAge: float
@@ -41,16 +42,21 @@ class HousingInput(BaseModel):
     Longitude: float
 
 # Load model
+
 def load_model():
     if os.path.exists(MODEL_PATH):
         return joblib.load(MODEL_PATH)
     return None
 
+
 model = load_model()
+
 
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": " Housing Price Prediction API (Flask)"}), 200
+
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -65,7 +71,7 @@ def predict():
         validated_input = HousingInput(**input_json)
         input_df = pd.DataFrame([validated_input.dict()])
         prediction = model.predict(input_df)[0]
-        logging.info(f" Prediction made | Input: {input_json} | Output: {prediction}")
+        logging.info(f"Prediction made|Input:{input_json}| Output:{prediction}")
         PREDICTION_COUNT.inc()
         return jsonify({"prediction": round(float(prediction), 4)}), 200
 
@@ -76,11 +82,14 @@ def predict():
     except Exception as e:
         ERROR_COUNT.inc()
         logging.exception(" Prediction error")
+	print(e)
         return jsonify({"error": "Prediction failed"}), 500
+
 
 @app.route("/metrics", methods=["GET"])
 def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 
 @app.route("/retrain", methods=["POST"])
 def retrain():
@@ -117,12 +126,14 @@ def retrain():
         model = new_model
 
         logging.info(f"Model retrained with {len(df)} records and saved.")
-        return jsonify({"message": f"Model retrained on {len(df)} samples."}), 200
+        return jsonify({"message":f"Model retrained on {len(df)} samples."}), 200
 
     except Exception as e:
         ERROR_COUNT.inc()
+	print(e)
         logging.exception("Retrain error")
         return jsonify({"error": "Retrain failed"}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
